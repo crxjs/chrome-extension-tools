58b7b0cda67a8797dc00e3b506f21fb78aa16616
Jack Steam <jacksteamdev@gmail.com>
2019-03-11 14:05:09 -0500

    install deriveEntries update rollup config ignore examples

--- src/index.js
+++ src/index.js
@@ -1,38 +1,12 @@
-import { watch } from 'rollup'
+import { deriveEntries } from '@bumble/manifest-entry-points'
 
-function watchAsync(config, cb) {
-  const watcher = watch(config)
-  const resolves = {
-    START: [],
-    BUNDLE_START: [],
-    BUNDLE_END: [],
-    END: [],
-    FATAL: [],
-    ERROR: [],
+export default function() {
+  return {
+    name: 'inputJson',
+    options({ input, ...options }) {
+      const entries = deriveEntries(input)
+
+      return { input: entries, ...options }
+    },
   }
-  const rejects = []
-
-  const eventHandler = event => {
-    const tuple = { value: event, done: event.code === 'FATAL' }
-
-    resolves[event.code].forEach(fn => fn(tuple))
-    resolves[event.code] = []
-
-    if (tuple.done) {
-      rejects.forEach(fn => fn(event))
-      watcher.close()
-    }
-  }
-
-  if (cb) watcher.on('event', cb)
-  watcher.on('event', eventHandler)
-
-  watcher.next = code =>
-    new Promise((resolve, reject) => {
-      resolves[code].push(resolve)
-      rejects.push(reject)
-    })
-  return watcher
 }
-
-export { watchAsync }
--- tests/fixtures/basic/rollup.config.js
+++ tests/fixtures/basic/rollup.config.js
@@ -1,9 +1,13 @@
 /* eslint-env node */
 
+import plugin from '../../../src/index'
+import { hooksInOrder } from '../../../examples/rollup-hook-order'
+
 export default {
-  input: 'tests/fixtures/basic/manifest.json',
+  input: 'tests/fixtures/basic/background.js',
   output: {
     dir: 'tests/fixtures/dest',
     format: 'iife',
   },
+  plugins: [plugin(), hooksInOrder()],
 }
