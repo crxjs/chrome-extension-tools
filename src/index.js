c3f7d66f24d9984f0f6dadf80b3cce1604761ef7
Jack Steam <jacksteamdev@gmail.com>
2019-03-12 08:53:01 -0500

    Add getScriptTags

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import { deriveEntries } from '@bumble/manifest-entry-points'
+import { readFileSync } from 'jsonfile'
+import path from 'path'
+import getScriptTags from '../src/script-tags'
 
 export default function() {
+  let assets
+
   return {
     name: 'inputJson',
-    options({ input, ...options }) {
-      const entries = deriveEntries(input)
 
-      return { input: entries, ...options }
+    options({ input, ...options }) {
+      const dirname = path.dirname(input)
+      const manifest = readFileSync(input)
+
+      const { js, css, html, img } = deriveEntries(manifest)
+      assets = [...css, ...img, ...html]
+
+      const inputs = [
+        ...js,
+        ...html.reduce(getScriptTags(dirname), []),
+      ]
+
+      return {
+        ...options,
+        input: inputs.map(name => path.join(dirname, name)),
+      }
+    },
+
+    buildStart() {
+      assets.map(asset => {
+        this.emitAsset(asset)
+      })
     },
   }
 }
