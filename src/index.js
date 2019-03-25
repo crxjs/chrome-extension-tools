036ecad514ff7a3c64938642afa4cd514826071e
Jack Steam <jacksteamdev@gmail.com>
2019-03-25 10:10:18 -0500

    Refactor composed plugin

--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
-import { deriveManifest, derivePermissions } from '@bumble/manifest'
-import composePlugins from './compose'
-import htmlInputs from './html-inputs'
+import {
+  deriveManifest,
+  derivePermissions,
+} from '@bumble/manifest'
+
+import htmlInputs from './html-inputs/index'
 import manifest from './manifest-input/index'
+import emptyOutputDir from './empty-output-dir/index'
 
 const transformManifest = pkg => (bundle, manifest) => {
-  const permissions = Object.values(bundle).reduce((set, { code }) => {
-    if (code) {
-      derivePermissions(code).forEach(prm => {
-        set.add(prm)
-      })
-    }
+  const permissions = Object.values(bundle).reduce(
+    (set, { code }) => {
+      if (code) {
+        derivePermissions(code).forEach(prm => {
+          set.add(prm)
+        })
+      }
 
-    return set
-  })
+      return set
+    },
+    new Set(),
+  )
 
   return deriveManifest(pkg, manifest, [...permissions])
 }
 
-const plugins = ({ pkg }) => [
+export default ({ pkg }) => [
   manifest({
     // manifest transform hook, called in writeBundle
     transform: transformManifest(pkg),
   }),
   htmlInputs(),
+  emptyOutputDir(),
 ]
-
-const name = 'chrome-extension'
-
-export default composePlugins(name, plugins)
