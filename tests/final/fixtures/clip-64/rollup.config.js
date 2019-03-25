f6306e679a4b1d99ee3d798474360187f70cae13
Jack Steam <jacksteamdev@gmail.com>
2019-03-25 14:55:21 -0500

    Update plugin info

--- tests/final/fixtures/clip-64/rollup.config.js
+++ tests/final/fixtures/clip-64/rollup.config.js
@@ -13,8 +13,8 @@ export default {
     format: 'esm',
   },
   plugins: [
-    ...chromeExtension({ pkg }),
+    chromeExtension({ pkg }),
     resolve(),
-    commonjs()
-  ],
+    commonjs(),
+  ].flat(),
 }
