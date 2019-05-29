5f5971741aeaa142e2e2f4cac5961b15c8fd9e10
Jack Steam <teamsteamdev@gmail.com>
2019-05-29 16:28:37 -0500

    Fix html inputs image getters

--- src/html-inputs/cheerio.js
+++ src/html-inputs/cheerio.js
@@ -2,29 +2,29 @@ import path from 'path'
 import fs from 'fs-extra'
 import cheerio from 'cheerio'
 
-export const loadHtml = filePath => {
+export const loadHtml = (filePath) => {
   const htmlCode = fs.readFileSync(filePath, 'utf8')
   const $ = cheerio.load(htmlCode)
 
   return $
 }
 
-const getRelativePath = htmlPath => p =>
+const getRelativePath = (htmlPath) => (p) =>
   path.join(path.dirname(htmlPath), p)
 
 export const getJsEntries = ([htmlPath, $]) =>
   $('script')
     .not('[data-rollup-asset]')
     .toArray()
-    .map(elem => $(elem).attr('src'))
+    .map((elem) => $(elem).attr('src'))
     .map(getRelativePath(htmlPath))
 
-export const mutateJsEntries = $ => {
+export const mutateJsEntries = ($) => {
   $('script')
     .not('[data-rollup-asset]')
     .toArray()
-    .map(elem => $(elem))
-    .forEach(e => {
+    .map((elem) => $(elem))
+    .forEach((e) => {
       e.attr('type', 'module')
     })
 
@@ -37,15 +37,15 @@ export const getJsAssets = ([htmlPath, $]) =>
   $('script')
     .filter('[data-rollup-asset="true"]')
     .toArray()
-    .map(elem => $(elem).attr('src'))
+    .map((elem) => $(elem).attr('src'))
     .map(getRelativePath(htmlPath))
 
 export const mutateJsAssets = ($, fn) => {
   $('script')
     .filter('[data-rollup-asset="true"]')
     .toArray()
-    .map(elem => $(elem))
-    .forEach(e => {
+    .map((elem) => $(elem))
+    .forEach((e) => {
       const value = fn(e.attr('src'))
       e.attr('src', value)
     })
@@ -59,15 +59,15 @@ export const getCssHrefs = ([htmlPath, $]) =>
   $('link')
     .filter('[rel="stylesheet"]')
     .toArray()
-    .map(elem => $(elem).attr('href'))
+    .map((elem) => $(elem).attr('href'))
     .map(getRelativePath(htmlPath))
 
 export const mutateCssHrefs = ($, fn) => {
   $('link')
     .filter('[rel="stylesheet"]')
     .toArray()
-    .map(elem => $(elem))
-    .forEach(e => {
+    .map((elem) => $(elem))
+    .forEach((e) => {
       const value = fn(e.attr('href'))
       e.attr('href', value)
     })
@@ -81,31 +81,43 @@ export const getImgSrcs = ([htmlPath, $]) =>
   [
     // get img tags
     ...$('img')
-      .not('[src|="http:-https:-data:-/"]')
+      .not('[src^="http://"]')
+      .not('[src^="https://"]')
+      .not('[src^="data://"]')
+      .not('[src^="/"]')
       .toArray()
-      .map(elem => $(elem).attr('src')),
+      .map((elem) => $(elem).attr('src')),
     // get favicons
     ...$('link[rel="icon"]')
-      .not('[href|="http:-https:-data:-/"]')
+      .not('[src^="http://"]')
+      .not('[src^="https://"]')
+      .not('[src^="data://"]')
+      .not('[src^="/"]')
       .toArray()
-      .map(elem => $(elem).attr('href')),
+      .map((elem) => $(elem).attr('href')),
   ].map(getRelativePath(htmlPath))
 
 export const mutateImgSrcs = ($, fn) => {
   $('img')
-    .not('[src|="http:-https:-data:-/"]')
+    .not('[src^="http://"]')
+    .not('[src^="https://"]')
+    .not('[src^="data://"]')
+    .not('[src^="/"]')
     .toArray()
-    .map(elem => $(elem))
-    .forEach(e => {
+    .map((elem) => $(elem))
+    .forEach((e) => {
       const value = fn(e.attr('src'))
       e.attr('src', value)
     })
 
   $('link[rel="icon"]')
-    .not('[href|="http:-https:-data:-/"]')
+    .not('[src^="http://"]')
+    .not('[src^="https://"]')
+    .not('[src^="data://"]')
+    .not('[src^="/"]')
     .toArray()
-    .map(elem => $(elem))
-    .forEach(e => {
+    .map((elem) => $(elem))
+    .forEach((e) => {
       const value = fn(e.attr('href'))
       e.attr('href', value)
     })
