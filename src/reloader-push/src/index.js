fb99de6ddae20ae50ae9f93d9a8a9e8cea70e964
Jack Steam <teamsteamdev@gmail.com>
2019-06-04 15:40:01 -0500

    Add start and updateManifest

--- src/reloader-push/src/index.js
+++ src/reloader-push/src/index.js
@@ -1,9 +1,25 @@
-// TODO: create ./firebase
-import { reloadClient } from './firebase'
+import { updateUserTime, loginAnonymously } from './config-index'
 import clientCode from './client.code'
 
+const state = {
+  // Anonymous UID from Firebase
+  // uid: string,
+  // Interval ID for updateUid
+  // interval: number,
+}
+
 export function start() {
-  // TODO: anon login to firebase
+  loginAnonymously()
+    .then((uid) => {
+      state.uid = uid
+
+      const update = updateUserTime(state)
+      state.interval = setInterval(update, 5 * 60 * 1000)
+    })
+    .catch((error) => {
+      console.log('Could not start push reloader')
+      console.error(error)
+    })
 }
 
 export function reload() {
@@ -13,6 +29,24 @@ export function reload() {
 
 export function getClientCode() {
   // TODO: configure client reloader code
-  // - replace uid's, etc...
+  // - replace %UID%
   return clientCode
+}
+
+export function updateManifest(manifest, path) {
+  // TODO: Update for push notifications
+  if (!manifest.background) {
+    manifest.background = {}
+  }
+
+  const { scripts = [] } = manifest.background
+
+  manifest.background.scripts = [...scripts, path]
+
+  if (manifest.background.persistent === undefined) {
+    manifest.background.persistent = false
+  }
+
+  manifest.description =
+    'DEVELOPMENT BUILD with auto-reloader script.'
 }
