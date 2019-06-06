a1fda38426772b8c06827cb2b07141f6c1a5becd
Jack Steam <teamsteamdev@gmail.com>
2019-06-06 10:34:21 -0500

    Test users.ts

--- src/reloader-push/functions/tests/users.test.ts
+++
@@ -1,5 +0,0 @@
-test.todo('cleanUpUsers removes inactive users from db')
-test.todo('cleanUpUsers deletes inactive user records')
-
-test.todo('setUserTime creates a user record')
-test.todo('setUserTime updates a user record')
