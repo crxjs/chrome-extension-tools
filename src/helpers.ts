550bfd256a5d123a368f4b4a8123fd7da7b64c2d
Jack Steam <teamsteamdev@gmail.com>
2019-10-04 11:33:56 -0500

    Convert to typescript

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,38 +1,9 @@
 import fs from 'fs-extra'
-import path from 'path'
-import { PluginContext } from 'rollup'
 
 export const not = (fn: any) => (x: any) => !fn(x)
 
 export const loadAssetData = (assetPath: string) =>
   fs.readFile(assetPath).then((src) => [assetPath, src])
 
-export const zipArrays = (a1: any[], a2: any[]) =>
+export const zipArrays = <T, X>(a1: T[], a2: X[]): [T, X][] =>
   a1.map((x, i) => [x, a2[i]])
-
-export async function getAssetPathMapFns(
-  this: PluginContext,
-  assets,
-) {
-  return (await assets).map(([assetPath, assetSrc]) => {
-    const name = path.basename(assetPath)
-    const id = this.emitAsset(name, assetSrc)
-    const assetFileName = this.getAssetFileName(id)
-
-    return (x) => {
-      if (typeof x !== 'string') return x
-
-      if (assetPath.endsWith(x)) {
-        return assetFileName
-      } else {
-        return x
-      }
-    }
-  })
-}
-
-export const writeFile = (dest) => ([htmlPath, htmlSrc]) => {
-  const baseName = path.basename(htmlPath)
-  const destPath = path.join(dest, baseName)
-  return fs.writeFile(destPath, htmlSrc)
-}
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,36 @@ import htmlInputs from './html-inputs/index'
 import manifestInput from './manifest-input/index'
 import useReloader from './reloader'
 import { validate as v } from './validate-names/index'
+import { PluginHooks } from 'rollup'
 
-export const chromeExtension = (opts) => {
-  const manifest = manifestInput(opts)
-  const html = htmlInputs(opts)
-  const reloader = useReloader(opts)
-  const validate = v(opts)
+export const chromeExtension = (options: {
+  assets?: {
+    include?: string[]
+    exclude?: string[]
+  }
+  dynamicImportWrapper?: {
+    wakeEvents?: string[]
+    eventDelay?: number | false
+  }
+  entries?: {
+    include?: string[]
+    exclude?: string[]
+  }
+  verbose?: boolean
+  pkg?: {
+    description: string
+    name: string
+    version: string
+  }
+  publicKey?: string
+  reloader?: 'non-persistent' | 'persistent'
+}): Partial<PluginHooks> & {
+  name: string
+} => {
+  const manifest = manifestInput(options)
+  const html = htmlInputs()
+  const reloader = useReloader(options)
+  const validate = v()
   const plugins = [manifest, html, reloader, validate]
 
   return {
@@ -18,6 +42,7 @@ export const chromeExtension = (opts) => {
 
       return plugins.reduce(
         (opts, plugin) =>
+          // @ts-ignore
           plugin[hook] ? plugin[hook].call(this, opts) : opts,
         options,
       )
@@ -26,29 +51,36 @@ export const chromeExtension = (opts) => {
     buildStart(options) {
       const hook = 'buildStart'
 
+      // @ts-ignore
       manifest[hook].call(this, options)
+      // @ts-ignore
       html[hook].call(this, options)
     },
 
     watchChange(id) {
       const hook = 'watchChange'
 
+      // @ts-ignore
       manifest[hook].call(this, id)
+      // @ts-ignore
       html[hook].call(this, id)
     },
 
     async generateBundle(...args) {
       const hook = 'generateBundle'
 
+      // @ts-ignore
       await manifest[hook].call(this, ...args)
-      await html[hook].call(this, ...args)
+      // @ts-ignore
       await reloader[hook].call(this, ...args)
+      // @ts-ignore
       await validate[hook].call(this, ...args)
     },
 
     async writeBundle(...args) {
       const hook = 'writeBundle'
 
+      // @ts-ignore
       await reloader[hook].call(this, ...args)
     },
   }
