eff04e193417e81a09cee0d5ec0d59744ac4a916
Jack Steam <teamsteamdev@gmail.com>
2019-11-20 09:30:56 -0600

    Add type guard

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,5 @@
 import fs from 'fs-extra'
+import { OutputChunk, OutputAsset } from 'rollup'
 
 export const not = <T>(fn: (x: T) => boolean) => (x: T) => !fn(x)
 
@@ -7,3 +8,9 @@ export const loadAssetData = (assetPath: string) =>
 
 export const zipArrays = <T, X>(a1: T[], a2: X[]): [T, X][] =>
   a1.map((x, i) => [x, a2[i]])
+
+export function isChunk(
+  x: OutputChunk | OutputAsset,
+): x is OutputChunk {
+  return x.type === 'chunk'
+}
