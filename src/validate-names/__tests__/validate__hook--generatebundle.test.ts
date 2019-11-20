a799d98e283e5a5ab1e350da61575c8597a71a97
Jack Steam <teamsteamdev@gmail.com>
2019-11-20 09:31:44 -0600

    Finish validate plugin tests

--- src/validate-names/__tests__/validate__hook--generatebundle.test.ts
+++ src/validate-names/__tests__/validate__hook--generatebundle.test.ts
@@ -1,7 +1,63 @@
-test.skip('modify bundle -mutate', () => {
-  // - all files that start with "_"
-  // - rename w/o "_"
-  // - Fix in manifest asset
-  // - Fix bundle key
-  // - fix in imports
+import { OutputBundle, OutputChunk } from 'rollup'
+import { context } from '../../../__fixtures__/plugin-context'
+import { isChunk } from '../../helpers'
+import { validate } from '../index'
+
+const bundle: OutputBundle = require('../../../__fixtures__/validate-names__sample-bundle.json')
+
+const helperKey = Object.keys(bundle).find((name) =>
+  name.includes('_'),
+)!
+
+const helperName = helperKey.split('-')[0]
+
+const fixedKey = helperKey.slice(1)
+const fixedName = helperName.slice(1)
+
+const plugin = validate()
+
+test('renames chunks by mutating the bundle', () => {
+  const helperChunk = bundle[helperKey] as OutputChunk
+
+  expect(helperChunk).toBeDefined()
+
+  {
+    const { facadeModuleId, fileName, name } = helperChunk
+
+    /* ------------------ PRE MUTATION ----------------- */
+    expect(fileName!).toMatch(helperKey)
+    expect(name!).toMatch(helperName)
+
+    expect(facadeModuleId!).toBeNull()
+  }
+
+  plugin.generateBundle.call(context, {}, bundle, false)
+
+  expect(bundle[helperKey]).toBeUndefined()
+
+  expect(bundle[fixedKey]).toBeDefined()
+  expect(bundle[fixedKey]).toBe(helperChunk)
+
+  {
+    const { facadeModuleId, fileName, name } = helperChunk
+
+    /* ----------------- POST MUTATION ----------------- */
+    expect(fileName!).not.toMatch(helperKey)
+    expect(name!).not.toMatch(helperKey)
+
+    expect(fileName!).toMatch(fixedKey)
+    expect(name!).toMatch(fixedName)
+
+    expect(facadeModuleId!).toBeNull()
+  }
+
+  const chunks = Object.values(bundle).filter(isChunk)
+
+  const dynamicImports = chunks.flatMap(
+    ({ dynamicImports: d }) => d,
+  )
+  const exports = chunks.flatMap(({ exports: e }) => e)
+
+  expect(dynamicImports).not.toContain(helperKey)
+  expect(exports).not.toContain(helperKey)
 })
