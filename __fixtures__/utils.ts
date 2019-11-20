805c97d7b940b94a1596db9649caa4cce5266c1d
Jack Steam <teamsteamdev@gmail.com>
2019-11-20 14:30:29 -0600

    Add helper fn

--- __fixtures__/utils.ts
+++ __fixtures__/utils.ts
@@ -2,3 +2,7 @@ import { resolve } from 'path'
 
 export const getExtPath = (path: string) =>
   resolve(__dirname, 'extensions', path)
+
+/**  Make relative to project root */
+export const getRelative = (p: string) =>
+  p.replace(process.cwd() + '/', '')
