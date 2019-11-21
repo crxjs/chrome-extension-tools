830c68213f951fc46ee42d2159120f4a57bfd675
Jack Steam <teamsteamdev@gmail.com>
2019-11-21 09:08:59 -0600

    Add html plugin init tests

--- src/html-inputs/__tests__/html__init.test.ts
+++ src/html-inputs/__tests__/html__init.test.ts
@@ -1,3 +1,12 @@
-test.skip('returns correct plugin', () => {
-  // TODO: name, options, buildStart, watchChange
+import htmlInputs from '../index'
+
+const plugin = htmlInputs({ srcDir: 'src' })
+
+test('returns correct plugin', () => {
+  expect(plugin).toMatchObject({
+    name: 'html-inputs',
+    options: expect.any(Function),
+    buildStart: expect.any(Function),
+    watchChange: expect.any(Function),
+  })
 })
