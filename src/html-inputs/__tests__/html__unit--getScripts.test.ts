b58786c6d5845c35c56059831e0653c37ae0ed5e
Jack Steam <teamsteamdev@gmail.com>
2019-11-21 10:44:07 -0600

    Add test for getScripts

--- src/html-inputs/__tests__/html__unit--getScripts.test.ts
+++ src/html-inputs/__tests__/html__unit--getScripts.test.ts
@@ -1,1 +1,15 @@
-test.todo('scrapes correct script tags')
+import { getExtPath } from '../../../__fixtures__/utils'
+import { loadHtml, getScriptSrc } from '../cheerio'
+
+const html$ = loadHtml(getExtPath('basic/options.html'))
+
+test('scrapes correct script tags', () => {
+  const result = getScriptSrc(html$)
+
+  expect(result).toEqual([
+    '__fixtures__/extensions/basic/options1.js',
+    '__fixtures__/extensions/basic/options2.jsx',
+    '__fixtures__/extensions/basic/options3.ts',
+    '__fixtures__/extensions/basic/options4.tsx',
+  ])
+})
