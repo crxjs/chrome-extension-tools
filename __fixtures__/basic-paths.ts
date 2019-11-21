d8cfe48760e7c8a7a671098a037433cc4a005154
Jack Steam <teamsteamdev@gmail.com>
2019-11-21 15:45:45 -0600

    Add more paths

--- __fixtures__/basic-paths.ts
+++ __fixtures__/basic-paths.ts
@@ -1,22 +1,25 @@
-import { getExtPath } from './utils';
+import { getExtPath } from './utils'
 
-export const optionsHtml = getExtPath('basic/options.html');
+export const srcDir = getExtPath('basic')
+export const manifestJson = getExtPath('basic/manifest.json')
 
-// Html scripts
-export const optionsJs = getExtPath('basic/options1.js');
-export const optionsJsx = getExtPath('basic/options2.jsx');
-export const optionsTs = getExtPath('basic/options3.ts');
-export const optionsTsx = getExtPath('basic/options4.tsx');
+// Options assets
+export const optionsHtml = getExtPath('basic/options.html')
+export const optionsCss = getExtPath('basic/options.css')
+export const optionsPng = getExtPath('basic/options.png')
+export const optionsJpg = getExtPath('basic/options.jpg')
+export const assetJs = getExtPath('basic/asset.js')
 
-// Html assets
-export const optionsCss = getExtPath('basic/options.css');
-export const optionsPng = getExtPath('basic/options.png');
-export const optionsJpg = getExtPath('basic/options.jpg');
-export const assetJs = getExtPath('basic/asset.js');
+// Options chunks
+export const optionsJs = getExtPath('basic/options1.js')
+export const optionsJsx = getExtPath('basic/options2.jsx')
+export const optionsTs = getExtPath('basic/options3.ts')
+export const optionsTsx = getExtPath('basic/options4.tsx')
 
-// External script file
-export const backgroundJs = getExtPath('basic/background.js');
+// External script files
+export const backgroundJs = getExtPath('basic/background.js')
+export const contentJs = getExtPath('basic/content.js')
 
 // Popup subfolder
-export const popupHtml = getExtPath('basic/popup/popup.html');
-export const popupJs = getExtPath('basic/popup/popup.js');
+export const popupHtml = getExtPath('basic/popup/popup.html')
+export const popupJs = getExtPath('basic/popup/popup.js')
