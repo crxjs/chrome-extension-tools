048ab25eaa32bd4632930a43f9749ac752b89ff7
Jack Steam <teamsteamdev@gmail.com>
2019-11-21 10:38:48 -0600

    Add tests for loadHtml

--- src/html-inputs/__tests__/html__unit--loadHtml.test.ts
+++ src/html-inputs/__tests__/html__unit--loadHtml.test.ts
@@ -1,2 +1,31 @@
-test.todo('calls readFileSync')
-test.todo('calls calls cheerio, load')
+import { getExtPath } from '../../../__fixtures__/utils'
+import { loadHtml } from '../cheerio'
+
+const fs = require('fs-extra')
+const cheerio = require('cheerio')
+
+jest.spyOn(fs, 'readFileSync')
+jest.spyOn(cheerio, 'load')
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+test('calls readFileSync', () => {
+  const filePath = getExtPath('basic/options.html')
+  const result = loadHtml(filePath)
+
+  expect(result).toBeInstanceOf(Function)
+  expect(result).toHaveProperty('filePath', filePath)
+
+  expect(fs.readFileSync).toBeCalledWith(filePath, 'utf8')
+})
+
+test('calls cheerio.load', () => {
+  const filePath = getExtPath('basic/options.html')
+  const htmlCode = fs.readFileSync(filePath, 'utf8')
+
+  loadHtml(filePath)
+
+  expect(cheerio.load).toBeCalledWith(htmlCode)
+})
