62e5d93972087ce781a00f0dc6a444ed14cbcbc2
Jack Steam <teamsteamdev@gmail.com>
2019-11-25 16:38:11 -0600

    Add test for main plugin init

--- src/__tests__/composed__init.test.ts
+++ src/__tests__/composed__init.test.ts
@@ -1,1 +1,27 @@
-test.todo('returns correct object')
+import { Plugin } from 'rollup'
+import { chromeExtension } from '..'
+
+test('returns correct object', () => {
+  const plugin = chromeExtension()
+
+  expect(plugin).toEqual({
+    name: 'chrome-extension',
+
+    _plugins: expect.objectContaining({
+      manifest: expect.objectContaining<Plugin>({
+        name: 'manifest-input',
+      }),
+      html: expect.objectContaining<Plugin>({
+        name: 'html-inputs',
+      }),
+      validate: expect.objectContaining<Plugin>({
+        name: 'validate-names',
+      }),
+    }),
+
+    options: expect.any(Function),
+    buildStart: expect.any(Function),
+    watchChange: expect.any(Function),
+    generateBundle: expect.any(Function),
+  })
+})
