f16b90c38cebf48a14871798e8a6205f9948775b
Jack Steam <teamsteamdev@gmail.com>
2019-11-26 16:48:03 -0600

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,18 +2,9 @@ module.exports = {
   moduleNameMapper: {
     'code .+': '<rootDir>/__fixtures__/bundle-imports-stub.ts',
   },
-  modulePathIgnorePatterns: [
-    '~~.+',
-    'push-reloader',
-    'simple-reloader',
-  ],
+  modulePathIgnorePatterns: ['~~.+'],
   preset: 'ts-jest',
   setupFilesAfterEnv: ['./jest.setup.ts'],
-  transform: {
-    // Not really sure what Sucrase does here,
-    // but Jest can't read the files without it.
-    '.(js|jsx|ts|tsx)': '@sucrase/jest-plugin',
-  },
   transformIgnorePatterns: [
     '<rootDir>/node_modules/(?!lodash-es/.*)',
   ],
