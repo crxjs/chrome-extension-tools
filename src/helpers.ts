c324a4396a946552e9e09b730b2da195677cebe7
Jack Steam <jacksteamdev@gmail.com>
2019-12-04 14:57:31 -0600

    Remove dead code

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,13 +1,6 @@
-import fs from 'fs-extra'
-import { OutputChunk, OutputAsset } from 'rollup'
+import { OutputAsset, OutputChunk } from 'rollup'
 
 export const not = <T>(fn: (x: T) => boolean) => (x: T) => !fn(x)
-
-export const loadAssetData = (assetPath: string) =>
-  fs.readFile(assetPath).then((src) => [assetPath, src])
-
-export const zipArrays = <T, X>(a1: T[], a2: X[]): [T, X][] =>
-  a1.map((x, i) => [x, a2[i]])
 
 export function isChunk(
   x: OutputChunk | OutputAsset,
