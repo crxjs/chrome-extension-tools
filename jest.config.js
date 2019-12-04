4cd6846e08864209be813026d00628f00f913c55
Jack Steam <teamsteamdev@gmail.com>
2019-12-04 10:17:05 -0600

    Add codecov

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,17 @@
 module.exports = {
+  coverageReporters: ['json-summary', 'text', 'lcov'],
   moduleNameMapper: {
     'code .+': '<rootDir>/__fixtures__/bundle-imports-stub.ts',
   },
   modulePathIgnorePatterns: ['~~.+'],
   preset: 'ts-jest',
   setupFilesAfterEnv: ['./jest.setup.ts'],
-  transformIgnorePatterns: [
-    '<rootDir>/node_modules/(?!lodash-es/.*)',
-  ],
   transform: {
     // Not really sure what Sucrase does here,
     // but Jest can't read the files without it.
     '.(js|jsx)': '@sucrase/jest-plugin',
   },
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!lodash-es/.*)',
+  ],
 }
