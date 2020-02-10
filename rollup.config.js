37dfce2c3ddc4ef1b5ae2085d534adba79569adc
Jack Steam <jacksteamdev@gmail.com>
2020-02-10 10:24:20 -0600

    Refactor rollup config.external

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,15 @@ import typescript from '@rollup/plugin-typescript'
 import bundleImports from 'rollup-plugin-bundle-imports'
 import json from '@rollup/plugin-json'
 
+const { dependencies } = require('./package.json')
+
+const external = Object.keys(dependencies).concat(
+  'firebase/app',
+  'firebase/auth',
+  'firebase/functions',
+  'path'
+)
+
 const {
   compilerOptions,
 } = require('./tsconfigs/tsconfig-base.json')
@@ -34,22 +43,7 @@ export default [
         sourcemap: 'inline',
       },
     ],
-    external: [
-      'ajv',
-      'cheerio',
-      'cosmiconfig',
-      'firebase/app',
-      'firebase/auth',
-      'firebase/functions',
-      'fs-extra',
-      'glob',
-      'lodash.difference',
-      'lodash.flatten',
-      'lodash.get',
-      'mem',
-      'path',
-      'slash',
-    ],
+    external,
     plugins,
   },
 ]
