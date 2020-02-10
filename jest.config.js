e0b4a2af5021536d39b16d4635d414e7bd10c856
Jack Steam <jacksteamdev@gmail.com>
2020-02-10 09:52:52 -0600

    Ignore module paths Disable test reporters

--- jest.config.js
+++ jest.config.js
@@ -6,13 +6,13 @@ module.exports = {
     '!src/push-reloader/client/**/*',
     '!src/simple-reloader/client/**/*',
   ],
-  coverageReporters: ['json-summary', 'text', 'lcov'],
+  // coverageReporters: ['json-summary', 'text', 'lcov'],
   moduleNameMapper: {
     'code .+': '<rootDir>/__fixtures__/bundle-imports-stub.ts',
   },
-  modulePathIgnorePatterns: ['~~.+'],
+  modulePathIgnorePatterns: ['~~.+', '__fixtures__', 'client'],
   preset: 'ts-jest',
-  reporters: ['default', 'jest-html-reporters'],
+  // reporters: ['default', 'jest-html-reporters'],
   setupFilesAfterEnv: ['./jest.setup.ts'],
   transform: {
     // Use Sucrase to convert ES6 modules in JS files
