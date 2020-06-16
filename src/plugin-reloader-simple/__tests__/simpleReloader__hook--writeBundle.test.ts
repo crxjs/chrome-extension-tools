ca2ca6d986dcbb6b4dbbb40afcb0008df5d9663c
Jack Steam <jacksteamdev@gmail.com>
2020-06-16 15:46:49 -0500

    Add tests for writeBundle

---
+++ src/plugin-reloader-simple/__tests__/simpleReloader__hook--writeBundle.test.ts
@@ -0,0 +1,58 @@
+import { outputJson } from 'fs-extra'
+import { join } from 'path'
+import { OutputBundle } from 'rollup'
+import {
+  simpleReloader,
+  SimpleReloaderPlugin,
+  _internalCache,
+} from '..'
+import { context } from '../../../__fixtures__/plugin-context'
+import { cloneObject } from '../../manifest-input/cloneObject'
+
+jest.mock('fs-extra', () => ({
+  outputJson: jest.fn(() => Promise.resolve()),
+}))
+
+const mockOutputJson = outputJson as jest.MockedFunction<
+  typeof outputJson
+>
+
+const outputDir = 'outputDir'
+const timestampPath = 'timestampPath'
+
+const originalBundle: OutputBundle = require('../../../__fixtures__/extensions/basic-bundle.json')
+
+let bundle: OutputBundle
+let plugin: SimpleReloaderPlugin
+beforeEach(() => {
+  process.env.ROLLUP_WATCH = 'true'
+
+  bundle = cloneObject(originalBundle)
+  plugin = simpleReloader({ outputDir, timestampPath })!
+})
+
+test('Writes timestamp file', async () => {
+  await plugin.writeBundle.call(context, bundle)
+
+  expect(mockOutputJson).toBeCalledWith(
+    join(outputDir, timestampPath),
+    expect.any(Number),
+  )
+})
+
+test('Handles write errors', async () => {
+  const message = 'ERROR!'
+
+  mockOutputJson.mockImplementation(() =>
+    Promise.reject({ message }),
+  )
+
+  // @ts-ignore
+  context.error.mockImplementationOnce(() => {})
+
+  await plugin.writeBundle.call(context, bundle)
+
+  expect(context.error).toBeCalledWith(
+    expect.stringContaining(message),
+  )
+})
