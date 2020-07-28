b2a63b586229d8128df4fa4c683e66ed9f90f799
Jack Steam <jacksteamdev@gmail.com>
2020-07-28 16:59:30 -0500

    Document getDeepMatches

--- src/manifest-input/browser/getDeepMatches.ts
+++ src/manifest-input/browser/getDeepMatches.ts
@@ -1,3 +1,13 @@
+/**
+ * Get matches from an object of nested objects
+ *
+ * @export
+ * @template T Type of matches
+ * @param {*} object Parent object to search
+ * @param {(x: any) => boolean} pred A predicate function that will receive each property value of an object
+ * @param {string[]} excludeKeys Exclude a property if the key exactly matches
+ * @returns {T[]} The matched values from the parent object
+ */
 export function getDeepMatches<T>(object: any, pred: (x: any) => boolean, excludeKeys: string[]): T[] {
   const keys = typeof object === 'object' ? Object.keys(object) : []
 
