62730291e8ce7385d41406cc367e1fb83f67ea5a
Jack Steam <jacksteamdev@gmail.com>
2020-09-11 17:49:21 -0500

    Do not double inject polyfills in executeScript

--- src/manifest-input/browser/executeScriptPolyfill.ts
+++ src/manifest-input/browser/executeScriptPolyfill.ts
@@ -1,17 +1,27 @@
-// Modify chrome.tabs.executeScript to inject browser polyfill
+const checkPolyfilled = `typeof browser !== undefined`
+
 const _executeScript = chrome.tabs.executeScript
+const withP = (...args: [any, any?]): Promise<any[]> =>
+  new Promise((resolve, reject) => {
+    _executeScript(...args, (results) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError.message)
+      } else {
+        resolve(results)
+      }
+    })
+  })
+
 chrome.tabs.executeScript = (...args: any): void => {
-  const tabId = typeof args[0] === 'number' ? args[0] : null
+  ;(async () => {
+    const baseArgs = (typeof args[0] === 'number' ? [args[0]] : []) as any[]
 
-  // execute browser polyfill
-  const polyfillArgs = (tabId === null ? ([] as any[]) : ([tabId] as any[])).concat([
-    // TODO: convert to file to get replacements right
-    { file: JSON.parse('%BROWSER_POLYFILL_PATH%') },
-    () => {
-      // execute original script
-      _executeScript(...(args as [any, any, any]))
-    },
-  ])
+    const [done] = await withP(...(baseArgs.concat({ code: checkPolyfilled }) as [any, any]))
 
-  _executeScript(...(polyfillArgs as [any, any, any]))
+    if (!done) {
+      await withP(...(baseArgs.concat([{ file: JSON.parse('%BROWSER_POLYFILL_PATH%') }]) as [any, any]))
+    }
+
+    _executeScript(...(args as [any, any, any]))
+  })()
 }
