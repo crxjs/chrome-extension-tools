8790575f42030d7f831401aa5889d674a4cb3f95
Jack Steam <jacksteamdev@gmail.com>
2020-10-26 14:10:05 -0500

    [tests] fix async beforeAll error

--- __tests__/e2e__not-recommended.test.ts
+++ __tests__/e2e__not-recommended.test.ts
@@ -1,50 +1,45 @@
 import { getExtPath, byFileName } from '../__fixtures__/utils'
 import { rollup, RollupOutput, OutputAsset } from 'rollup'
 import { ChromeExtensionManifest } from '../src/manifest'
+import { inversePromise } from '../__fixtures__/inversePromise'
 
-const { default: config } = require(getExtPath(
-  'not-recommended/rollup.config.js',
-))
+const { default: config } = require(getExtPath('not-recommended/rollup.config.js'))
 
-// const manifestJson = getExtPath('not-recommended/manifest.json')
-// const contentCss = getExtPath('not-recommended/content.css')
-// const backgroundHtml = getExtPath(
-//   'not-recommended/background.html',
-// )
-
-let output: RollupOutput['output']
+const outputPromise = inversePromise<RollupOutput['output']>()
 beforeAll(async () => {
-  const bundle = await rollup(config)
-  const { output: o } = await bundle.generate(config.output)
-  output = o
-})
+  try {
+    const bundle = await rollup(config)
+    const { output } = await bundle.generate(config.output)
+    outputPromise.resolve(output)
+  } catch (error) {
+    outputPromise.reject(error)
+  }
+}, 10000)
 
-test('Handles background html page', () => {
-  const backgroundHtml = output.find(
-    byFileName('background.html'),
-  )
+test('Handles background html page', async () => {
+  const output = await outputPromise
+
+  const backgroundHtml = output.find(byFileName('background.html'))
   const backgroundJs = output.find(byFileName('background.js'))
 
   expect(backgroundHtml).toBeDefined()
   expect(backgroundJs).toBeDefined()
 })
 
-test('Handles content scripts with only css', () => {
+test('Handles content scripts with only css', async () => {
+  const output = await outputPromise
+
   const contentCss = output.find(byFileName('content.css'))
 
   expect(contentCss).toBeDefined()
 })
 
-test('Handles CSP in manifest.json', () => {
-  const manifestJson = output.find(
-    byFileName('manifest.json'),
-  ) as OutputAsset
+test('Handles CSP in manifest.json', async () => {
+  const output = await outputPromise
 
-  const manifest = JSON.parse(
-    manifestJson.source as string,
-  ) as ChromeExtensionManifest
+  const manifestJson = output.find(byFileName('manifest.json')) as OutputAsset
 
-  expect(manifest.content_security_policy).toBe(
-    "script-src 'self'; object-src 'self'",
-  )
+  const manifest = JSON.parse(manifestJson.source as string) as ChromeExtensionManifest
+
+  expect(manifest.content_security_policy).toBe("script-src 'self'; object-src 'self'")
 })
