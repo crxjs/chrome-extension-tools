8944987284b71c4b56b4d211b3aeac8a922c5eae
Jack Steam <jacksteamdev@gmail.com>
2020-10-26 17:03:20 -0500

    [tests] remove typescript plugin from fixtures

--- __fixtures__/extensions/no-scripts/rollup.config.js
+++ __fixtures__/extensions/no-scripts/rollup.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-import typescript from '@rollup/plugin-typescript'
 import { chromeExtension } from '../../../src/index'
 import { getExtPath } from '../../utils'
 
@@ -13,7 +12,6 @@ export default {
   },
   plugins: [
     chromeExtension({ verbose: false }),
-    typescript(),
     resolve(),
     commonjs(),
   ],
