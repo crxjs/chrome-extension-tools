8d48d1523db2f89dda05bd775ceb10440fa7673d
Jack Steam <jacksteamdev@gmail.com>
2021-03-01 09:49:25 -0600

    tests: add basic ts test

---
+++ __fixtures__/extensions/basic-ts/src/pages/popup/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Popup Page</h1>
+      <p>If you are seeing this, React is working!</p>
+    </div>
+  )
+}
+
+export default App
