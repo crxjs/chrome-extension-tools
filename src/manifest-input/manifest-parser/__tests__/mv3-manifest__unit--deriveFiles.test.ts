c60557c62941749eadd2e857a663d50937dcd889
Jack Steam <jacksteamdev@gmail.com>
2021-08-05 17:15:13 -0500

    feat: parse mv3

---
+++ src/manifest-input/manifest-parser/__tests__/mv3-manifest__unit--deriveFiles.test.ts
@@ -0,0 +1,60 @@
+import { readJSONSync } from 'fs-extra'
+import { deriveFiles } from '..'
+import {
+  contentCss,
+  contentJs,
+  devtoolsHtml,
+  icon128,
+  icon16,
+  icon48,
+  manifestJson,
+  optionsHtml,
+  popupHtml,
+  serviceWorkerJs,
+  srcDir,
+} from '../../../../__fixtures__/mv3-kitchen-sink-paths'
+
+const manifest = readJSONSync(manifestJson)
+
+test('gets correct scripts', () => {
+  const result = deriveFiles(manifest, srcDir)
+
+  expect(result.js).toContain(serviceWorkerJs)
+  expect(result.js).toContain(contentJs)
+})
+
+test('gets correct html', () => {
+  const result = deriveFiles(manifest, srcDir)
+
+  expect(result.html).toContain(optionsHtml)
+  expect(result.html).toContain(popupHtml)
+  expect(result.html).toContain(devtoolsHtml)
+})
+
+test('gets correct css', () => {
+  const result = deriveFiles(manifest, srcDir)
+
+  expect(result.css).toContain(contentCss)
+})
+
+test('gets correct action icon', () => {
+  const result = deriveFiles(manifest, srcDir)
+
+  expect(result.img).toContain(icon16)
+})
+
+test('gets correct action img', () => {
+  const result = deriveFiles(manifest, srcDir)
+
+  expect(result.img).toContain(icon16)
+  expect(result.img).toContain(icon48)
+  expect(result.img).toContain(icon128)
+})
+
+test('does not emit duplicates', () => {
+  const result = deriveFiles(manifest, srcDir)
+
+  expect(result.js.length).toBe(2)
+  expect(result.js).toContain(serviceWorkerJs)
+  expect(result.js).toContain(contentJs)
+})
--- src/manifest-input/manifest-parser/index.ts
+++ src/manifest-input/manifest-parser/index.ts
@@ -5,6 +5,7 @@ import { join } from 'path'
 import { OutputChunk } from 'rollup'
 import * as permissions from './permissions'
 import { ContentScript } from '../../manifest-types'
+import { isString } from '../../helpers'
 
 /* ============================================ */
 /*              DERIVE PERMISSIONS              */
@@ -19,22 +20,6 @@ export const derivePermissions = (
     .map(([key]) => key)
     .reduce((s, p) => s.add(p), set)
 
-// /* ============================================ */
-// /*                DERIVE MANIFEST               */
-// /* ============================================ */
-
-// export function deriveManifest(
-//   manifest: chrome.runtime.ManifestV2, // manifest.json
-//   ...permissions: string[] | string[][] // will be combined with manifest.permissions
-// ): chrome.runtime.ManifestV2 {
-//   return validateManifest({
-//     // SMELL: Is this necessary?
-//     manifest_version: 2,
-//     ...manifest,
-//     permissions: combinePerms(permissions, manifest.permissions),
-//   })
-// }
-
 /* -------------------------------------------- */
 /*                 DERIVE FILES                 */
 /* -------------------------------------------- */
@@ -44,10 +29,94 @@ export function deriveFiles(
   srcDir: string,
 ) {
   if (manifest.manifest_version === 3) {
-    // TODO: support deriving files from MV3
-    throw new Error('MV3 is unsupported')
+    return deriveFilesMV3(manifest, srcDir)
+  } else {
+    return deriveFilesMV2(manifest, srcDir)
+  }
+}
+
+export function deriveFilesMV3(
+  manifest: chrome.runtime.ManifestV3,
+  srcDir: string,
+) {
+  const files = get(
+    manifest,
+    'web_accessible_resources',
+    [] as Required<typeof manifest>['web_accessible_resources'],
+  )
+    .flatMap(({ resources }) => resources)
+    .reduce((r, x) => {
+      if (glob.hasMagic(x)) {
+        const files = glob.sync(x, { cwd: srcDir })
+        return [...r, ...files.map((f) => f.replace(srcDir, ''))]
+      } else {
+        return [...r, x]
+      }
+    }, [] as string[])
+
+  const js = [
+    ...files.filter((f) => /\.[jt]sx?$/.test(f)),
+    get(manifest, 'background.service_worker'),
+    ...get(
+      manifest,
+      'content_scripts',
+      [] as ContentScript[],
+    ).reduce((r, { js = [] }) => [...r, ...js], [] as string[]),
+  ]
+
+  const html = [
+    ...files.filter((f) => /\.html?$/.test(f)),
+    get(manifest, 'options_page'),
+    get(manifest, 'options_ui.page'),
+    get(manifest, 'devtools_page'),
+    get(manifest, 'action.default_popup'),
+    ...Object.values(get(manifest, 'chrome_url_overrides', {})),
+  ]
+
+  const css = [
+    ...files.filter((f) => f.endsWith('.css')),
+    ...get(
+      manifest,
+      'content_scripts',
+      [] as ContentScript[],
+    ).reduce(
+      (r, { css = [] }) => [...r, ...css],
+      [] as string[],
+    ),
+  ]
+
+  const img = [
+    ...files.filter((f) =>
+      /\.(jpe?g|png|svg|tiff?|gif|webp|bmp|ico)$/i.test(f),
+    ),
+    ...(Object.values(get(manifest, 'icons', {})) as string[]),
+    ...(Object.values(
+      get(manifest, 'action.default_icon', {}),
+    ) as string[]),
+  ]
+
+  // Files like fonts, things that are not expected
+  const others = diff(files, css, js, html, img)
+
+  return {
+    css: validate(css),
+    js: validate(js),
+    html: validate(html),
+    img: validate(img),
+    others: validate(others),
   }
 
+  function validate(ary: any[]) {
+    return [...new Set(ary.filter(isString))].map((x) =>
+      join(srcDir, x),
+    )
+  }
+}
+
+export function deriveFilesMV2(
+  manifest: chrome.runtime.ManifestV2,
+  srcDir: string,
+) {
   const files = get(
     manifest,
     'web_accessible_resources',
@@ -94,7 +163,6 @@ export function deriveFiles(
     ),
   ]
 
-  // TODO: this can be a string or object
   const actionIconSet = [
     'browser_action.default_icon',
     'page_action.default_icon',
@@ -137,9 +205,5 @@ export function deriveFiles(
     return [...new Set(ary.filter(isString))].map((x) =>
       join(srcDir, x),
     )
-  }
-
-  function isString(x: any): x is string {
-    return typeof x === 'string'
   }
 }
