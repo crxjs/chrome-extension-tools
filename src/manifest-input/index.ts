36b3db7514c1aa2040cea788b4a2db13e0fa37b7
Jack Steam <jacksteamdev@gmail.com>
2021-08-10 17:52:26 -0500

    docs: add deprecations and deprecation warnings

--- src/manifest-input/index.ts
+++ src/manifest-input/index.ts
@@ -347,8 +347,47 @@ export function manifestInput(
         this.emitFile(asset)
       })
 
+      /* ------------ WARN DEPRECATED OPTIONS ------------ */
+
+      if (crossBrowser)
+        this.warn(
+          '`options.crossBrowser` is not implemented yet',
+        )
+
+      if (!firstClassManifest)
+        this.warn(
+          '`options.firstClassManifest` will be removed in version 5.0.0',
+        )
+
+      if (iifeJsonPaths.length)
+        this.warn(
+          '`options.iifeJsonPaths` is not implemented yet',
+        )
+
       // MV2 manifest is handled in `generateBundle`
       if (isMV2(cache.manifest)) return
+
+      if (browserPolyfill)
+        this.warn(
+          [
+            '`options.browserPolyfill` is deprecated for MV3 and does nothing internally',
+            'See: https://extend-chrome.dev/rollup-plugin#mv3-faq',
+          ].join('\n'),
+        )
+
+      if (dynamicImportWrapper)
+        this.warn(
+          '`options.dynamicImportWrapper` is not required for MV3',
+        )
+
+      if (publicKey)
+        this.warn(
+          [
+            '`options.publicKey` is deprecated for MV3,',
+            'please use `options.extendManifest` instead',
+            'see: https://extend-chrome.dev/rollup-plugin#mv3-faq',
+          ].join('\n'),
+        )
 
       /* --------------- EMIT MV3 MANIFEST --------------- */
 
