e39767e79aa7902fb112d99ed7a759ae1c67aa9b
Jack Steam <jacksteamdev@gmail.com>
2021-08-31 14:39:02 -0500

    chore: fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,23 @@
 /* eslint-env node */
 
 // import typescript from '@rollup/plugin-typescript'
-import sucrase from '@rollup/plugin-sucrase'
-import bundleImports from 'rollup-plugin-bundle-imports'
 import json from '@rollup/plugin-json'
+import sucrase from '@rollup/plugin-sucrase'
+import fs from 'fs-extra'
+import path from 'path'
+import bundleImports from 'rollup-plugin-bundle-imports'
 
-const { dependencies } = require('./package.json')
+const { dependencies } = fs.readJsonSync(
+  path.join(process.cwd(), 'package.json'),
+)
 
-const external = Object.keys(dependencies).concat('path', 'fs')
+const external = Object.keys(dependencies).concat(
+  'fs',
+  'path',
+  'rollup',
+  'xstate/lib/actions',
+  'xstate/lib/model',
+)
 
 const plugins = [
   json(),
