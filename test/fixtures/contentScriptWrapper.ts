b3651ff3e1e250b4053da20c676ee4351e2b57ec
Jack Steam <jacksteamdev@gmail.com>
2021-08-31 15:20:45 -0500

    tests: make real bundle imports fixtures

---
+++ test/fixtures/contentScriptWrapper.ts
@@ -0,0 +1,2 @@
+import { code } from 'code ../../src/manifest-input/browser/contentScriptWrapper.ts'
+export { code }
---
+++ test/fixtures/importWrapper--explicit.ts
@@ -0,0 +1,2 @@
+import { code } from 'code ../../src/manifest-input/browser/importWrapper--explicit.ts'
+export { code }
