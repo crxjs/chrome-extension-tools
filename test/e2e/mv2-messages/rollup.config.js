d8b37a2f4f95731f1075d89002f5e2a10469eea0
Jack Steam <jacksteamdev@gmail.com>
2021-09-06 11:41:36 -0500

    tests: add mv2 e2e tests

---
+++ test/e2e/mv2-messages/rollup.config.js
@@ -0,0 +1,30 @@
+import { chromeExtension, simpleReloader } from '$src'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import path from 'path'
+
+export const outDir = path.join(__dirname, 'dist-rollup-build')
+
+export default {
+  input: path.join(__dirname, 'src', 'manifest.json'),
+  output: {
+    dir: outDir,
+    format: 'esm',
+    chunkFileNames: 'chunks/[name]-[hash].js',
+    sourcemap: 'inline',
+  },
+  plugins: [
+    chromeExtension(),
+    // Adds a Chrome extension reloader during watch mode
+    simpleReloader(),
+    resolve(),
+    commonjs({
+      namedExports: {
+        react: Object.keys(require('react')),
+        'react-dom': Object.keys(require('react-dom')),
+      },
+    }),
+    typescript(),
+  ],
+}
---
+++ test/e2e/mv2-messages/src/messages.ts
@@ -0,0 +1,14 @@
+import { getMessage } from '@extend-chrome/messages'
+
+export const [sendBgCheck, bgCheckStream] = getMessage<
+  undefined,
+  Pick<chrome.tabs.Tab, 'id'>
+>('background check', { async: true })
+
+export const [sendBgOk, bgOkStream] = getMessage<undefined>(
+  'background ok',
+)
+
+export const [sendOptOk, optOkStream] = getMessage<undefined>(
+  'options ok',
+)
