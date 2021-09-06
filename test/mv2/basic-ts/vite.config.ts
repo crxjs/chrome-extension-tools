dcbe27b465e7082d1154232e993b7cfcfca36084
Jack Steam <jacksteamdev@gmail.com>
2021-09-06 14:21:13 -0500

    tests: add basic-ts vite tests

---
+++ test/mv2/basic-ts/vite.config.ts
@@ -0,0 +1,17 @@
+import { chromeExtension } from '$src'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: path.join(__dirname, 'src'),
+  clearScreen: false,
+  logLevel: 'error',
+  build: {
+    minify: false,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.join(__dirname, 'src', 'manifest.json'),
+    },
+  },
+  plugins: [chromeExtension()],
+})
