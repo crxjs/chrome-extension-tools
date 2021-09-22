b646ff364bfce9331b15c27965ea1a68872e59b7
Jack Steam <jacksteamdev@gmail.com>
2021-09-22 13:28:25 -0500

    chore: fix rollup path resolution

--- rollup.config.js
+++ rollup.config.js
@@ -3,23 +3,33 @@
 // import typescript from '@rollup/plugin-typescript'
 import json from '@rollup/plugin-json'
 import sucrase from '@rollup/plugin-sucrase'
+import alias from '@rollup/plugin-alias'
 import fs from 'fs-extra'
 import path from 'path'
 import bundleImports from 'rollup-plugin-bundle-imports'
 
-const { dependencies } = fs.readJsonSync(
+const { dependencies, peerDependencies = {} } = fs.readJsonSync(
   path.join(process.cwd(), 'package.json'),
 )
 
-const external = Object.keys(dependencies).concat(
-  'fs',
-  'path',
-  'rollup',
-  'xstate/lib/actions',
-  'xstate/lib/model',
-)
+const external = Object.keys({
+  ...dependencies,
+  ...peerDependencies,
+}).concat('fs', 'path', 'xstate/lib/actions', 'xstate/lib/model')
 
 const plugins = [
+  alias({
+    entries: [
+      {
+        find: /^\$src\/(.*)/,
+        replacement: path.resolve(__dirname, 'src/$1'),
+      },
+      {
+        find: '$src',
+        replacement: path.resolve(__dirname, 'src', 'index.ts'),
+      },
+    ],
+  }),
   json(),
   sucrase({
     transforms: ['typescript'],
