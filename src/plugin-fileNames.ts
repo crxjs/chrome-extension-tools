39423b4ece8875d34506eab0b603ab5ec681b7a9
Jack Steam <jacksteamdev@gmail.com>
2021-09-29 16:17:18 -0500

    refactor: rename builtin

--- src/plugin-fileNames.ts
+++ src/plugin-fileNames.ts
@@ -4,8 +4,12 @@ export const chunkFileNames = 'modules/[name]-[hash].js'
 export const entryFileNames = '[name].js'
 export const assetFileNames = 'assets/[name]-[hash].[ext]'
 
-export const viteSupport = (): RPCEPlugin => ({
-  name: 'vite-support',
+/**
+ * Vite sets its own opinionated file names that don't work for a Chrome extension
+ * This overwrites them with file names that make sense for our purposes
+ */
+export const fileNames = (): RPCEPlugin => ({
+  name: 'file-names',
   outputOptions(options) {
     // Entries must be in original location
     return {
