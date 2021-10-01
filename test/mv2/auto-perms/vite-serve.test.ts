cfd5c5b9ea2624e3458efd4760d1fca83adc4d7d
Jack Steam <jacksteamdev@gmail.com>
2021-10-01 15:23:51 -0500

    feat: add auto permission detection

---
+++ test/mv2/auto-perms/vite-serve.test.ts
@@ -0,0 +1,35 @@
+import { fileWriteComplete } from '$src/viteAdaptor'
+import fs from 'fs-extra'
+import path from 'path'
+import { createServer, ViteDevServer } from 'vite'
+
+process.chdir(__dirname)
+
+const outDir = path.join(__dirname, 'dist-serve')
+
+let devServer: ViteDevServer
+beforeAll(async () => {
+  await fs.remove(outDir)
+
+  devServer = await createServer({
+    configFile: 'vite.config.ts',
+    envFile: false,
+    build: { outDir },
+  })
+})
+
+afterAll(async () => {
+  await devServer.close()
+})
+
+test.skip('writes entry points to disk', async () => {
+  await Promise.all([devServer.listen(), fileWriteComplete()])
+
+  const manifest = 'manifest.json'
+  const manifestPath = path.join(outDir, manifest)
+  const manifestSource = await fs.readJson(manifestPath)
+
+  expect(manifestSource).toMatchObject({
+    permissions: ['notifications'],
+  })
+})
