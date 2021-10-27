0198fce3198915dcf8d0b1d57e5c2177cf11c192
Jack Steam <jacksteamdev@gmail.com>
2021-10-27 11:15:19 -0500

    docs: add comment

--- src/plugin-fileNames.ts
+++ src/plugin-fileNames.ts
@@ -7,6 +7,7 @@ export const assetFileNames = 'assets/[name]-[hash].[ext]'
 /**
  * Vite sets its own opinionated file names that don't work for a Chrome extension
  * This overwrites them with file names that make sense for our purposes
+ * TODO: explore relaxing this constraint
  */
 export const fileNames = (): RPCEPlugin => ({
   name: 'file-names',
