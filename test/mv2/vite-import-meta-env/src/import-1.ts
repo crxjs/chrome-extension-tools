3b21f225a40691254c1bf6e24f6e68878617ec6e
Jack Steam <jacksteamdev@gmail.com>
2021-11-19 17:45:20 -0600

    fix: remove vite import meta env workaround

--- test/mv2/vite-import-meta-env/src/import-1.ts
+++
@@ -1,2 +0,0 @@
-export const a = import.meta.env.VITE_ENV_A
-export const b = import.meta.env.VITE_ENV_B
--- test/mv2/vite-import-meta-env/src/import-2.ts
+++
@@ -1,2 +0,0 @@
-export const c = import.meta.env.VITE_ENV_C
-export const d = import.meta.env.VITE_ENV_D
