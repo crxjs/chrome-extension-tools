35237964546251147f254ec2cae70cd28e22db71
Jack Steam <jacksteamdev@gmail.com>
2021-12-03 14:11:21 -0600

    fix: remove unused import

--- src/index.ts
+++ src/index.ts
@@ -11,16 +11,12 @@ import {
   not,
 } from './helpers'
 import { runPlugins } from './index_runPlugins'
-import { basename, isAbsolute, join, parse } from './path'
+import { basename, isAbsolute, join } from './path'
 import { autoPerms } from './plugin-autoPerms'
 import { browserPolyfill } from './plugin-browserPolyfill'
 import { configureRollupOptions } from './plugin-configureRollupOptions'
-import { viteServeHMR_MV2 } from './plugin-viteServeHMR_MV2'
-import { viteServeHMR_MV3 } from './plugin-viteServeHMR_MV3'
 import { esmBackground } from './plugin-esmBackground'
 import { extendManifest } from './plugin-extendManifest'
-import { viteServeReactFastRefresh_MV2 } from './plugin-viteServeReactFastRefresh_MV2'
-import { viteServeReactFastRefresh_MV3 } from './plugin-viteServeReactFastRefresh_MV3'
 import { htmlMapScriptsToJS } from './plugin-htmlMapScriptsToJS'
 import { hybridFormat } from './plugin-hybridOutput'
 import { packageJson } from './plugin-packageJson'
@@ -30,6 +26,10 @@ import {
   validateManifest,
 } from './plugin-validateManifest'
 import { viteServeFileWriter } from './plugin-viteServeFileWriter'
+import { viteServeHMR_MV2 } from './plugin-viteServeHMR_MV2'
+import { viteServeHMR_MV3 } from './plugin-viteServeHMR_MV3'
+import { viteServeReactFastRefresh_MV2 } from './plugin-viteServeReactFastRefresh_MV2'
+import { viteServeReactFastRefresh_MV3 } from './plugin-viteServeReactFastRefresh_MV3'
 import { xstateCompat } from './plugin-xstateCompat'
 import {
   combinePlugins,
