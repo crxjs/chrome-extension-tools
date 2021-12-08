0cd48934166966f8db1cbc2a5d9762aead8afe61
Jack Steam <jacksteamdev@gmail.com>
2021-12-08 08:48:31 -0600

    tests: add html inputs test

---
+++ test/mv3/vite-html-inputs/vite.config.ts
@@ -0,0 +1,18 @@
+import { chromeExtension } from '$src'
+import { defineConfig } from 'vite'
+
+process.chdir(__dirname)
+
+export default defineConfig({
+  root: 'src',
+  clearScreen: false,
+  logLevel: 'error',
+  build: {
+    minify: false,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: ['test1.html', 'test2.html'],
+    },
+  },
+  plugins: [chromeExtension()],
+})
