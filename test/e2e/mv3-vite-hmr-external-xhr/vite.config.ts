d1d794c69e6d66c685ab31bd2232e0bb06e0caa0
Zach Bruggeman <mail@bruggie.com>
2021-12-14 14:58:52 -0800

    Add e2e test to ensure external XHR works

---
+++ test/e2e/mv3-vite-hmr-external-xhr/vite.config.ts
@@ -0,0 +1,20 @@
+import { chromeExtension } from '$src'
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig(() => ({
+  root: path.join(__dirname, 'src'),
+  clearScreen: false,
+  logLevel: 'error',
+  build: {
+    minify: false,
+    emptyOutDir: true,
+    sourcemap: 'inline',
+  },
+  plugins: [chromeExtension(), react()],
+  cacheDir: path.join(__dirname, '.vite'),
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
+}))
