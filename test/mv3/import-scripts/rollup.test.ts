b9ef1b38e3825d2321bbd1d7d3e27afc074cf382
Jack Steam <jacksteamdev@gmail.com>
2022-01-21 10:55:57 -0600

    tests: use template on mv3 rollup tests

--- test/mv3/import-scripts/rollup.test.ts
+++ test/mv3/import-scripts/rollup.test.ts
@@ -1,40 +1,12 @@
-import { parseManifest } from '$src/files_parseManifest'
-import { isAsset, isChunk } from '$src/helpers'
-import { getRollupOutput } from '$test/helpers/getRollupOutput'
+import {
+  getRollupOutput,
+  testBuildOutput,
+} from '$test/helpers/testBuild'
 import { jestSetTimeout } from '$test/helpers/timeout'
-import { byFileName } from '$test/helpers/utils'
-import { OutputAsset } from 'rollup'
 
 jestSetTimeout(30000)
 
-test('bundles chunks and assets', async () => {
-  const { output } = await getRollupOutput(
-    __dirname,
-    'rollup.config.js',
-  )
-
-  const manifest = 'manifest.json'
-  const manifestAsset = output.find(
-    byFileName(manifest),
-  ) as OutputAsset
-  expect(manifestAsset).toBeDefined()
-  const manifestSource = JSON.parse(
-    manifestAsset.source as string,
-  ) as chrome.runtime.Manifest
-  expect(manifestSource).toMatchSnapshot(manifest)
-
-  const parsed = Object.values(parseManifest(manifestSource))
-  expect(parsed).toMatchSnapshot('parsed manifest')
-
-  const files = Object.values(parsed).flatMap((x) => x)
-  for (const filename of files) {
-    const file = output.find(byFileName(filename))!
-    const source = isChunk(file) ? file.code : file.source
-    expect(source).toMatchSnapshot(filename)
-  }
-
-  // 4 scripts
-  expect(output.filter(isChunk).length).toBe(4)
-  // content script wrapper + manifest
-  expect(output.filter(isAsset).length).toBe(2)
+test('manifest vs output', async () => {
+  const output = await getRollupOutput(__dirname)
+  await testBuildOutput(output)
 })
--- test/mv3/invalid-manifest/rollup.test.ts
+++ test/mv3/invalid-manifest/rollup.test.ts
@@ -1,4 +1,3 @@
-import { format } from '$src/helpers'
 import { rollup, RollupOptions } from 'rollup'
 import config from './rollup.config'
 
@@ -6,8 +5,7 @@ process.chdir(__dirname)
 
 test('errors if the manifest is invalid', async () => {
   expect.assertions(1)
-  await expect(rollup(config as RollupOptions)).rejects.toEqual(
-    new Error(format`There were problems with the extension manifest.
-                     - ["service_worker.js"] at "manifest.background.service_worker" must be string`),
-  )
+  await expect(
+    rollup(config as RollupOptions),
+  ).rejects.toMatchSnapshot('error')
 })
