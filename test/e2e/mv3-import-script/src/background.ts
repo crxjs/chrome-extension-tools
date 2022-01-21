28efdf7235b9ddebfc23a41c890b04e85e98ff30
Jack Steam <jacksteamdev@gmail.com>
2022-01-21 16:08:13 -0600

    chore: fix types for background service workers

--- test/e2e/mv3-import-script/src/background.ts
+++ test/e2e/mv3-import-script/src/background.ts
@@ -1,4 +1,9 @@
-chrome.runtime.onInstalled.addListener(({ reason }) => {
+// This fixes `self`'s type.
+declare const self: ServiceWorkerGlobalScope
+export {}
+
+chrome.runtime.onInstalled.addListener(async ({ reason }) => {
+  await self.skipWaiting()
   if (reason === chrome.runtime.OnInstalledReason.INSTALL)
     chrome.runtime.openOptionsPage()
 })
