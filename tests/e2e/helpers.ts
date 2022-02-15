d44b2d856782ddc2fe807b1edcf9211893d70206
Jack Steam <jacksteamdev@gmail.com>
2022-02-14 18:27:05 -0600

    tests: add getPage debug logs for gha

--- tests/e2e/helpers.ts
+++ tests/e2e/helpers.ts
@@ -9,15 +9,18 @@ export async function getPage(
 
   console.log('get page', included)
   while (!page && count < 50) {
+    const pages = browserContext.pages()
     console.log(
       'pages',
-      browserContext.pages().map((p) => p.url()),
+      pages.map((p) => p.url()),
     )
-    page = browserContext.pages().find((p) => p.url().includes(included))
+    page = pages.find((p) => p.url().includes(included))
+    console.log('page', page)
     if (!page) await new Promise((r) => setTimeout(r, 100))
     count++
   }
 
+  console.log('get page loop done', page)
   if (!page) throw new Error(`Could not get page "${included}"`)
   return page
 }
