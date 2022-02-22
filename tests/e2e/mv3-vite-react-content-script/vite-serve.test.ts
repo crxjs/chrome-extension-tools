d6a8fb431fb9f03e1568e347d5f5ee8bca0725c1
Jack Steam <jacksteamdev@gmail.com>
2022-02-21 18:09:07 -0600

    tests: adjust tests

--- tests/e2e/mv3-vite-react-content-script/vite-serve.test.ts
+++ tests/e2e/mv3-vite-react-content-script/vite-serve.test.ts
@@ -4,11 +4,10 @@ test('crx runs from server output', async () => {
   const { browser } = await serve(__dirname)
   const page = await browser.newPage()
   await page.goto('https://www.google.com')
-
-  await new Promise(() => null)
-
   await page.emulateMedia({ reducedMotion: 'reduce' })
-  await page.waitForSelector('.App')
 
-  expect(await page.screenshot()).toMatchImageSnapshot()
+  const app = page.locator('.App')
+  await app.waitFor()
+
+  expect(await app.screenshot()).toMatchImageSnapshot()
 })
