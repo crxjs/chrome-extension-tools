909daa5bd1ddeddd193535042328b94428cf9204
Jack Steam <jacksteamdev@gmail.com>
2022-03-01 16:27:41 -0600

    tests: clean up fixtures

--- tests/e2e/mv3-dynamic-script/src/content2.ts
+++ tests/e2e/mv3-dynamic-script/src/content2.ts
@@ -2,8 +2,7 @@ import fileName from './main-world?script&module'
 
 const url = chrome.runtime.getURL(fileName)
 
-console.log('content.ts')
-console.log('main world script url', url)
+console.log('content2.ts')
 
 const script = document.createElement('script')
 script.src = url
--- tests/mv3/import-scripts/src/declared-script.ts
+++ tests/mv3/import-scripts/src/declared-script.ts
@@ -1,7 +1,8 @@
-import fileName from './main-world?script'
+import fileName from './main-world?script&module'
 
 console.log('declared script')
 
 const script = document.createElement('script')
 script.src = chrome.runtime.getURL(fileName)
+script.type = 'module'
 document.head.prepend(script)
