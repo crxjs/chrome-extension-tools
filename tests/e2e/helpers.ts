c6ea5f6cae7752ecec54faba1d2852385c5e5488
Jack Steam <jacksteamdev@gmail.com>
2022-03-02 17:01:37 -0600

    tests: refactor getPage helper

--- tests/e2e/helpers.ts
+++ tests/e2e/helpers.ts
@@ -1,21 +1,20 @@
 import { BrowserContext, Page } from 'playwright-chromium'
 
-export async function getPage(browserContext: BrowserContext, test: string) {
-  let count = 0
-  let page: Page | undefined
-  let pages: Page[] = []
-
-  while (!page && count < 50) {
-    pages = browserContext.pages()
-    page = pages.find((p) => p.url().includes(test))
-    if (!page) await new Promise((r) => setTimeout(r, 100))
-    count++
-  }
-
-  if (!page) {
-    console.log({ included: test, page, pages: pages.map((p) => p.url()) })
-    throw new Error(`Could not get page "${test}"`)
-  }
+export async function getPage(
+  browser: BrowserContext,
+  test: RegExp | string,
+): Promise<Page> {
+  const regex = new RegExp(test)
+  const page = await Promise.race([
+    browser.waitForEvent('page', async (p) => {
+      await p.waitForURL(regex)
+      return true
+    }),
+    ...browser.pages().map(async (p) => {
+      await p.waitForURL(regex)
+      return p
+    }),
+  ])
 
   return page
 }
