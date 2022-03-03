7fc3217390980dae0bb5b70165ab3ae143a72244
Jack Steam <jacksteamdev@gmail.com>
2022-03-03 10:40:59 -0600

    tests: fix input type

--- tests/e2e/helpers.ts
+++ tests/e2e/helpers.ts
@@ -1,10 +1,11 @@
+import jsesc from 'jsesc'
 import { BrowserContext, Page } from 'playwright-chromium'
 
 export async function getPage(
   browser: BrowserContext,
   test: RegExp | string,
 ): Promise<Page> {
-  const regex = new RegExp(test)
+  const regex = test instanceof RegExp ? test : new RegExp(jsesc(test))
   const page = await Promise.race([
     browser.waitForEvent('page', async (p) => {
       await p.waitForURL(regex)
