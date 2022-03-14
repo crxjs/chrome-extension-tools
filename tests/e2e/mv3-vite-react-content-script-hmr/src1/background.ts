bc3a4b29cdeb2d75cfb4341fd54190d4808e93b3
Jack Steam <jacksteamdev@gmail.com>
2022-03-14 11:24:10 -0500

    tests: simplify extension background code

--- tests/e2e/mv3-vite-react-content-script-hmr/src1/background.ts
+++ tests/e2e/mv3-vite-react-content-script-hmr/src1/background.ts
@@ -1,9 +1,2 @@
-// This fixes `self`'s type.
-declare const self: ServiceWorkerGlobalScope
+chrome.runtime.openOptionsPage()
 export {}
-;(async () => {
-  await self.skipWaiting()
-  // if we open the options page now the SW won't reroute fetches
-  await new Promise((r) => setTimeout(r, 100))
-  chrome.runtime.openOptionsPage()
-})()
