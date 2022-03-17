c029a4d3fc8cc9ad61a0b02669cad55a9a6dca18
Jack Steam <jacksteamdev@gmail.com>
2022-03-17 12:00:33 -0500

    tests: remove custom diff dir

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,12 +1,10 @@
 import { configureToMatchImageSnapshot } from 'jest-image-snapshot'
-import { resolve } from 'src/path'
 
 expect.extend({
   toMatchImageSnapshot: configureToMatchImageSnapshot({
     comparisonMethod: 'ssim',
     failureThreshold: 0.02,
     failureThresholdType: 'percent',
-    customDiffDir: resolve(__dirname, '__image_snapshot_diffs__'),
     allowSizeMismatch: true,
   }),
 })
