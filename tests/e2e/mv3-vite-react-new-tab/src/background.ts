df83fdac13f2657d6f571a3e954540ca4f6c9d88
Jack Steam <jacksteamdev@gmail.com>
2022-04-07 17:04:12 -0500

    tests: update crx background file

--- tests/e2e/mv3-vite-react-new-tab/src/background.ts
+++ tests/e2e/mv3-vite-react-new-tab/src/background.ts
@@ -1,9 +1,3 @@
-// This fixes `self`'s type.
-declare const self: ServiceWorkerGlobalScope
+// this should open a new tab
+chrome.tabs.create({})
 export {}
-;(async () => {
-  await self.skipWaiting()
-  // if we open the options page now the SW won't reroute fetches
-  await new Promise((r) => setTimeout(r, 100))
-  chrome.runtime.openOptionsPage()
-})()
