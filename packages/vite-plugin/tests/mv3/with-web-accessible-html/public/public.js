aa8ea2830520f8271780b252240aae908042a355
Jack Steam <jacksteamdev@gmail.com>
2022-05-26 17:01:46 -0500

    HTML files as web accessible resources (#386)
    * tests: add html resources test extension
    
    * tests: add static asset
    
    * tests: add use cases
    
    * tests: add snapshots
    
    * tests: update snapshots

---
+++ packages/vite-plugin/tests/mv3/with-web-accessible-html/public/public.js
@@ -0,0 +1,1 @@
+console.log('static script')
---
+++ packages/vite-plugin/tests/mv3/with-web-accessible-html/vite.config.ts
@@ -0,0 +1,50 @@
+import { crx } from 'src/.'
+import { join } from 'src/path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    minify: false,
+    rollupOptions: {
+      // these html files support HMR and Vite production optimizations
+      input: {
+        // this file is web accessible (see manifest.web_accessible_resources)
+        sidebar: join(__dirname, 'src/sidebar.html'),
+        // this file is not web accessible
+        welcome: join(__dirname, 'src/welcome.html'),
+      },
+      output: {
+        // the hash randomly changes between environments
+        assetFileNames: 'assets/[name].hash.[ext]',
+        chunkFileNames: 'assets/[name].hash.js',
+        entryFileNames: 'assets/[name].hash.js',
+      },
+    },
+  },
+  clearScreen: false,
+  logLevel: 'error',
+  plugins: [
+    crx({
+      manifest: {
+        manifest_version: 3,
+        version: '1.0.0',
+        name: 'Extension with copied assets',
+        web_accessible_resources: [
+          {
+            resources: [
+              // this file is web accessible; it supports HMR b/c it's declared in `rollupOptions.input`
+              'src/sidebar.html',
+              // this static HTML file is web accessible; CRXJS copies it directly (static assets should be in public dir)
+              'src/static.html',
+              // this static HTML file in `public/` is web accessible; the path works b/c it's in the public dir
+              'public.html',
+              // If you're using a content script to load a web accessible HTML file in an iframe,
+              // the assets of the web accessible HTMl file don't need to be web accessible 🤯
+            ],
+            matches: ['https://www.google.com/*'],
+          },
+        ],
+      },
+    }),
+  ],
+})
