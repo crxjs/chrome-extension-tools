07ff699a152a60798cb2523eada81a90d633dba7
Jack Steam <jacksteamdev@gmail.com>
2022-05-26 15:02:30 -0500

    tests: add html resources test extension

---
+++ packages/vite-plugin/tests/mv3/with-web-accessible-html/vite.config.ts
@@ -0,0 +1,46 @@
+import { crx } from 'src/.'
+import { join } from 'src/path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    minify: false,
+    rollupOptions: {
+      // these html files support HMR and Vite production optimizations
+      input: {
+        // this file is web accessible (see manifest.web_accessible_resources)
+        sidebar: join(__dirname, 'src/sidebar.html'),
+        // this file is not web accessible
+        welcome: join(__dirname, 'src/welcome.html'),
+      },
+      output: {
+        // the hash randomly changes between environments
+        assetFileNames: 'assets/[name].hash.[ext]',
+        chunkFileNames: 'assets/[name].hash.js',
+        entryFileNames: 'assets/[name].hash.js',
+      },
+    },
+  },
+  clearScreen: false,
+  logLevel: 'error',
+  plugins: [
+    crx({
+      manifest: {
+        manifest_version: 3,
+        version: '1.0.0',
+        name: 'Extension with copied assets',
+        web_accessible_resources: [
+          {
+            resources: [
+              // this file is web accessible; it supports HMR b/c it's declared in `rollupOptions.input`
+              'src/sidebar.html',
+              // this file is web accessible; CRXJS copies it directly
+              'src/static.html',
+            ],
+            matches: ['https://www.google.com/*'],
+          },
+        ],
+      },
+    }),
+  ],
+})
