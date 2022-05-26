f222afbe0169dc1789d76f5aa39478e8a037214a
Jack Steam <jacksteamdev@gmail.com>
2022-05-26 11:08:20 -0500

    fix: watch public dir, not each public file

--- packages/vite-plugin/src/node/plugin-fileWriter--public.ts
+++ packages/vite-plugin/src/node/plugin-fileWriter--public.ts
@@ -14,14 +14,14 @@ export const pluginFileWriterPublic: CrxPluginFn = () => {
     },
     async buildStart() {
       if (this.meta.watchMode) {
+        // rebuild when files in public dir are added or changed
+        // Rollup feature PR: https://github.com/rollup/rollup/pull/3812
         this.addWatchFile(config.publicDir)
         const publicFiles = await glob(`${config.publicDir}/**/*`)
         for (const file of publicFiles) {
-          // TODO: cache public files
           const source = await readFile(file)
           const fileName = relative(config.publicDir, file)
           this.emitFile({ type: 'asset', fileName, source })
-          this.addWatchFile(file)
         }
       }
     },
