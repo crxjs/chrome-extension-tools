6c072f2bd9be2931450b1e00525654db7e8914c3
Jack Steam <jacksteamdev@gmail.com>
2022-05-30 16:03:14 -0500

    tests: add unit test

---
+++ packages/vite-plugin/src/node/stubMatchPattern.test.ts
@@ -0,0 +1,25 @@
+import { stubMatchPattern } from './helpers'
+
+test.each`
+  pattern                          | expected
+  ${'*://*/*foo.json*'}            | ${'*://*/*'}
+  ${'*://google.*'}                | ${'*://google.*'}
+  ${'http://*.google.com/search*'} | ${'http://*.google.com/*'}
+  ${'http://b.com'}                | ${'http://b.com'}
+  ${'https://a.com/*'}             | ${'https://a.com/*'}
+  ${'https://a.com/subpath/* '}    | ${'https://a.com/*'}
+`(
+  '$pattern -> $expected',
+  ({ pattern, expected }: { pattern: string; expected: string }) => {
+    const result = stubMatchPattern(pattern)
+    expect(result).toBe(expected)
+  },
+)
+
+test('sorting mystery', () => {
+  const inputs = ['https://a.com/*', 'http://b.com/*']
+  const outputs = inputs.map(stubMatchPattern)
+
+  expect(inputs).toEqual(outputs)
+  expect(inputs.sort()).toEqual(outputs.sort())
+})
