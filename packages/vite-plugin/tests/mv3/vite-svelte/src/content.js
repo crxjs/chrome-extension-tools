a84d4dd881b07ad04f291704f76f2b380d503f51
Jack Steam <jacksteamdev@gmail.com>
2022-06-06 14:12:01 -0500

    tests: fix missing param

--- packages/vite-plugin/tests/mv3/vite-svelte/src/content.js
+++ packages/vite-plugin/tests/mv3/vite-svelte/src/content.js
@@ -1,7 +1,8 @@
 import App from './App.svelte'
 
 const html = `<div id="crx-app"></div>`
-const target = new DOMParser().parseFromString(html).body.firstElementChild
+const target = new DOMParser().parseFromString(html, 'text/html').body
+  .firstElementChild
 document.body.append(target)
 
 const app = new App({ target })
