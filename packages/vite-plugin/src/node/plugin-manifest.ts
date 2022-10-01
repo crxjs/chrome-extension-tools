a22884002792095faef19f1f22d119f64621b2aa
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-10-01 04:48:55 -0300

    fixing problem paths not resolving correctly

--- packages/vite-plugin/src/node/plugin-manifest.ts
+++ packages/vite-plugin/src/node/plugin-manifest.ts
@@ -1,5 +1,6 @@
 import { existsSync } from 'fs'
 import { promises as fs } from 'fs'
+import { resolve } from 'path/posix'
 import colors from 'picocolors'
 import { OutputAsset, OutputChunk } from 'rollup'
 import { ResolvedConfig } from 'vite'
@@ -185,11 +186,10 @@ export const pluginManifest =
                 const refJS = js.map((file) =>
                   this.emitFile({
                     type: 'chunk',
-                    id: file,
+                    id: resolve(config.root, file),
                     name: basename(file),
                   }),
                 )
-
                 return { js: refJS, ...rest }
               },
             )
@@ -201,7 +201,7 @@ export const pluginManifest =
               const file = manifest.background.service_worker
               const refId = this.emitFile({
                 type: 'chunk',
-                id: file,
+                id: resolve(config.root, file),
                 name: basename(file),
               })
               manifest.background.service_worker = refId
@@ -210,7 +210,7 @@ export const pluginManifest =
             for (const file of htmlFiles(manifest)) {
               this.emitFile({
                 type: 'chunk',
-                id: file,
+                id: resolve(config.root, file),
                 name: basename(file),
               })
             }
