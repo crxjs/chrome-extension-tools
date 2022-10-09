03b6aa63b21e9e097c6e35906deaf077a425f178
Jack Steam <jacksteamdev@gmail.com>
2022-10-09 09:23:12 -0500

    tests: assert hashScriptId returns a stable hash

---
+++ packages/vite-plugin/src/node/hashScriptId.test.ts
@@ -0,0 +1,9 @@
+import { hashScriptId } from './contentScripts'
+
+test('hashScriptId returns a stable hash', async () => {
+  const hashes = new Set<string>()
+  for (let count = 0; count < 5; count++) {
+    hashes.add(hashScriptId({ type: 'module', id: 'abc123' }))
+  }
+  expect(hashes.size).toBe(1)
+})
