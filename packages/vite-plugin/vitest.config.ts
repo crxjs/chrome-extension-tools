ae50bd3cdb047e5429c44a876395ca85792d0b32
Jack Steam <jacksteamdev@gmail.com>
2022-10-09 09:27:10 -0500

    tests: add e2e and ci test scripts

--- packages/vite-plugin/vitest.config.ts
+++ packages/vite-plugin/vitest.config.ts
@@ -6,27 +6,33 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 console.log(__dirname)
 
-export default defineConfig({
-  test: {
-    exclude: ['**/templates/**', '**/node_modules/**'],
-    globalSetup: './tests/jest.globalSetup.ts',
-    setupFiles: './tests/jest.setup.ts',
-    alias: [
-      { find: 'src', replacement: path.resolve(__dirname, 'src/node') },
-      { find: 'tests', replacement: path.resolve(__dirname, 'tests') },
-      {
-        find: /^client\/(.+)\.ts$/,
-        replacement: `${path.resolve(__dirname, 'tests/artifacts')}/$1.js`,
+export default defineConfig(({ mode }) => {
+  const isE2E = mode === 'e2e'
+  const exclude = isE2E ? ['**/src/**', '**/mv3/**'] : ['**/e2e/**']
+  return {
+    test: {
+      alias: [
+        { find: 'src', replacement: path.resolve(__dirname, 'src/node') },
+        { find: 'tests', replacement: path.resolve(__dirname, 'tests') },
+        {
+          find: /^client\/(.+)\.ts$/,
+          replacement: `${path.resolve(__dirname, 'tests/artifacts')}/$1.js`,
+        },
+        {
+          find: /^client\/(.+)\.html$/,
+          replacement: `${path.resolve(__dirname, 'tests/artifacts')}/$1.js`,
+        },
+      ],
+      exclude: [...exclude, '**/templates/**', '**/node_modules/**'],
+      globalSetup: './tests/jest.globalSetup.ts',
+      maxThreads: isE2E ? 1 : undefined,
+      minThreads: isE2E ? 1 : undefined,
+      setupFiles: './tests/jest.setup.ts',
+      snapshotFormat: {
+        printBasicPrototype: true,
       },
-      {
-        find: /^client\/(.+)\.html$/,
-        replacement: `${path.resolve(__dirname, 'tests/artifacts')}/$1.js`,
-      },
-    ],
-    watchExclude: [...configDefaults.watchExclude, '**/tests/templates'],
-    testTimeout: process.env.TIMEOUT ? parseInt(process.env.TIMEOUT) : 30000,
-    snapshotFormat: {
-      printBasicPrototype: true,
+      testTimeout: process.env.TIMEOUT ? parseInt(process.env.TIMEOUT) : 30000,
+      watchExclude: [...configDefaults.watchExclude, '**/tests/templates'],
     },
-  },
+  }
 })
