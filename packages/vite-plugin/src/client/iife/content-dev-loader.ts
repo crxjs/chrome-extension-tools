a4982e362b15f99b7ce6d10a2348d0deb555ad90
Jack Steam <jacksteamdev@gmail.com>
2023-04-16 22:10:56 -0500

    Add content script module API (#689)
    * implement feature
    
    * update output tests
    
    * move type declaraction file
    
    * move types into client types
    
    * add e2e test
    
    * Create strong-jobs-try.md
    
    * change name of exported function
    
    * do not auto-format client iife scripts
    
    * update output tests
    
    * preserve entry module signature
    
    * update tests

--- packages/vite-plugin/src/client/iife/content-dev-loader.ts
+++ packages/vite-plugin/src/client/iife/content-dev-loader.ts
@@ -1,12 +1,15 @@
 declare const __PREAMBLE__: string
 declare const __CLIENT__: string
 declare const __SCRIPT__: string
+const injectTime = performance.now()
 ;(async () => {
   // remember, __PREAMBLE__ and __CLIENT__ are emitted files
   if (__PREAMBLE__)
     await import(/* @vite-ignore */ chrome.runtime.getURL(__PREAMBLE__))
   await import(/* @vite-ignore */ chrome.runtime.getURL(__CLIENT__))
-  await import(/* @vite-ignore */ chrome.runtime.getURL(__SCRIPT__))
+  const { onExecute } = await import(/* @vite-ignore */ chrome.runtime.getURL(__SCRIPT__)) as ContentScriptAPI.ModuleExports
+  // this is the entry point of the content script, it will run each time this script is injected
+  onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } })
 })().catch(console.error)
 
 export {}
