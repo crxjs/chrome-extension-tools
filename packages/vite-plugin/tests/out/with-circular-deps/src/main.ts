aa7964aee8b608e0c19aea375061d6ffea87f85e
Jack Steam <jacksteamdev@gmail.com>
2023-06-18 17:13:33 -0500

    fix: linting error

--- packages/vite-plugin/tests/out/with-circular-deps/src/main.ts
+++ packages/vite-plugin/tests/out/with-circular-deps/src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { setupCounter } from './counter'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
