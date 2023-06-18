77f2d4aecc3a94d96212e4c390162ebbb1cf7bbc
Jack Steam <jacksteamdev@gmail.com>
2023-06-18 13:58:42 -0500

    test: fix vite config

--- packages/vite-plugin/tests/out/with-circular-deps/vite.config.ts
+++ packages/vite-plugin/tests/out/with-circular-deps/vite.config.ts
@@ -1,9 +1,21 @@
-import { crx } from "@crxjs/vite-plugin";
-import { defineConfig } from 'vite';
-import manifest from "./manifest.json";
+import { crx } from '../../plugin-testOptionsProvider'
+import { defineConfig } from 'vite'
+import manifest from './manifest.json'
 
 export default defineConfig({
-    plugins: [
-        crx({ manifest }),
-    ]
-});
+  build: {
+    sourcemap: 'inline',
+    minify: false,
+    rollupOptions: {
+      output: {
+        // the hash randomly changes between environments
+        assetFileNames: 'assets/[name].hash[hash].[ext]',
+        chunkFileNames: 'assets/[name].hash[hash].js',
+        entryFileNames: 'assets/[name].hash[hash].js',
+      },
+    },
+  },
+  clearScreen: false,
+  logLevel: 'error',
+  plugins: [crx({ manifest })],
+})
