906168c825fffcef208a4e5ea2cce55fede91e4d
Tomasz Dłuski <mr.toumash@gmail.com>
2025-05-31 22:37:06 +0200

    fix: unreliable hmr fs watcher on windows ci. Taken from vitejs/vite repo
    https://github.com/vitejs/vite/blob/54e0a18773be6f6c35d92db5be7f3e6159c23589/playground/vitestSetup.ts#L215

--- packages/vite-plugin/tests/runners.ts
+++ packages/vite-plugin/tests/runners.ts
@@ -130,6 +130,10 @@ export async function serve(dirname: string): Promise<ServeTestResult> {
       watch: {
         // cache dir should not trigger update in these tests
         ignored: [cacheDir],
+        // During tests we edit the files too fast and sometimes chokidar
+        // misses change events, so enforce polling for consistency
+        usePolling: true,
+        interval: 100,
       },
     },
   }
