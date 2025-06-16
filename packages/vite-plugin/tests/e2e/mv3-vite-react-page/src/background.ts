341d5b72b5e659c83ee15b2f4a009e583427eb6c
Tomasz Dłuski <mr.toumash@gmail.com>
2025-06-16 15:33:59 +0200

    refactor(tests): remove all skipWaiting since they have no effects in e2e testing

--- packages/vite-plugin/tests/e2e/mv3-vite-react-page/src/background.ts
+++ packages/vite-plugin/tests/e2e/mv3-vite-react-page/src/background.ts
@@ -2,7 +2,6 @@
 declare const self: ServiceWorkerGlobalScope
 export {}
 ;(async () => {
-  await self.skipWaiting()
   // if we open the options page now the SW won't reroute fetches
   await new Promise((r) => setTimeout(r, 100))
   chrome.runtime.openOptionsPage()
