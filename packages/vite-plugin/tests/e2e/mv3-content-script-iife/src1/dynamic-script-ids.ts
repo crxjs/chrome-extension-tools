d4534b6e47b7efecc8a7a0769d2ab73f09d289c9
Salmin Skenderovic <sskenderovic@proofpoint.com>
2026-06-05 19:13:06 -0400

    abstract script ids into resulable consts

---
+++ packages/vite-plugin/tests/e2e/mv3-content-script-iife/src1/dynamic-script-ids.ts
@@ -0,0 +1,3 @@
+export const dynamicRegularId = 'dynamic-regular' as const
+export const dynamicIifeId = 'dynamic-iife' as const
+export const dynamicBareIifeAliasId = 'dynamic-bare-iife-alias' as const
